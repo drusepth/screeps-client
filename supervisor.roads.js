7106fe9dde9811a7bd4168b5c240c944b4a296eb
Andrew Brown <drusepth@gmail.com>
2017-10-28 01:46:20 +0300

    Release: Sat Oct 28 01:46:20 EEST 2017

--- supervisor.roads.js
+++ supervisor.roads.js
@@ -1,7 +1,7 @@
 module.exports = {
   constructEnergyRoads: function (spawnerName) {
     this.spawner = Game.spawns[spawnerName];
-    this.room    = spawner.room;
+    this.room    = this.spawner.room;
 
     var energySources = this.energySourcesInRoom(this.room);
     for (var i = 0; i < energySources.length; i++) {
